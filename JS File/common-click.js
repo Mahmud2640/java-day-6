b3d2a0699f1f624b95787b0f89d527d6d06832a1
Mahmud2640 <mdmahmud484@gmail.com>
2022-02-12 10:18:32 +0600

    add some code

--- JS File/common-click.js
+++ JS File/common-click.js
@@ -1,4 +1,9 @@
 function changeP() {
-  const textChange = document.getElementById('text');
-  textChange.innerText = 'hello brooooo';
+  const textChange = document.getElementById("text");
+  textChange.innerText = "hello brooooo";
 }
+
+document.getElementById("submit-area").addEventListener("click", function () {
+  const addText = document.getElementById("input-area");
+  const addpost = addText.Value;
+});
